fcbf7774a7dbbabd1b1a75abeadad0685a3f212e
Maxim Kikenia <job.mk9@gmail.com>
2023-10-23 12:26:52 +0300

    random icon app test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
+import { useState, useRef } from 'react';
 import './App.css';
+import { iconsNames } from './iconsNames';
+
 
 function App() {
+  const [icon, setIcon] = useState(false);
+  const refStatus = useRef();
+
+
+  function getRandomInt() {
+    let min = 2
+    let max = 1558
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function createIcon() {
+    refStatus.current.innerHTML = "Загружается...";
+    setTimeout(() => {
+      setIcon(getRandomInt());
+      refStatus.current.innerHTML = "";
+    }, 3000);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="AppWrapper">
+      <div className="App">
+        <h1>Генератор иконок FontAwesome</h1>
+        <p ref={refStatus}></p>
+        <i className={!icon ? iconsNames[getRandomInt()] : iconsNames[icon]}></i><br />
+        <button onClick={createIcon} type='button'>Загрузить</button>
+      </div>
     </div>
   );
 }
